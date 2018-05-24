16190eab3cb129efff05413233e761b454c857db
Timothy O'Connor <tim@oconnorinformatics.com>
2018-05-24 07:18:29 -0500

    Removed debug stack tracing becuase it works only on mac :

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 
 #include <memory>
 
-#include <execinfo.h>
-
 #include "BasicStatsCollector.h"
 
 using namespace std;
@@ -29,24 +27,9 @@ static unsigned int firstUpdateRate;
 static int qualHistLowerVal;
 static int qualHistUpperVal;
 
-void handler(int sig) {
-  void *array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
-
 void printStatsJansson(AbstractStatCollector* rootStatCollector);
 
 int main(int argc, char* argv[]) {
-
-	signal(SIGSEGV, handler);   // install our handler
 
 	string filename;
 	updateRate = 1000;
